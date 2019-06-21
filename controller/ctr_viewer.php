3bf4b2a1c6cc86cbbf623788676ef7beb897ae91
Dictor <kimdictor@gmail.com>
2019-06-21 14:23:23 +0000

    viewer 디자인 개선 및 페이지 표시 기능 추가

--- controller/ctr_viewer.php
+++ controller/ctr_viewer.php
@@ -5,23 +5,37 @@
 
 	class ctr_viewer {
 		public static function ShowImage($bookid, $pagenum) {
-			if(empty($_GET['page']) or is_int($_GET['page'])){
+			if(empty($pagenum) or is_int($pagenum)){
 				echo '<img class="filled-image" onclick=location.href="./viewer.php?book_id='.$bookid.'&page=2" src=./image.php?book_id='.$bookid.'&page=1>';
 			} else {
 				if(!mdl_book::InitSqlite()) return;
-				$res = mdl_book::SearchBook($_GET['book_id']);
+				$res = mdl_book::SearchBook($bookid);
 				mdl_book::CloseSqlite();
 				if (count($res) == 0) {
 					echo '404 Not Found';
 				} else {
 					$pages = library::GetEntry($res[0]->path);
-					if(count($pages) == (int)$_GET['page']){
+					if(count($pages) == (int)$pagenum){
 						echo '<img class="filled-image" src=./image.php?book_id='.$bookid.'&page='.$pagenum.'>';
 					} else {
 						echo '<img class="filled-image" onclick=location.href="./viewer.php?book_id='.$bookid.'&page='.($pagenum + 1).'" src=./image.php?book_id='.$bookid.'&page='.$pagenum.'>';
 					}
 				}
 			}
-		}	
+		}
+		
+		public static function ShowInfo($book_id, $pagenum) {
+			if(!mdl_book::InitSqlite()) return;
+			$res = mdl_book::SearchBook($book_id);
+			mdl_book::CloseSqlite();
+
+			if (count($res) == 0) {
+				$txt = '404';
+			} else {
+				$txt = $pagenum.'/'.(string)count(library::GetEntry($res[0]->path));
+			}
+			
+			echo '<div class="book-info">'.$txt.'</div>';
+		}
 	}
 ?>
--- viewer.php
+++ viewer.php
@@ -14,7 +14,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
-	<body>
+	<body class="viewer">
 		<div class="container">
 			<?php
 				require_once 'controller/ctr_viewer.php';
@@ -24,10 +24,12 @@
 				} else {
 					if (empty($_GET['page'])) {
 						ctr_viewer::ShowImage($_GET['book_id'], '1');
+						ctr_viewer::ShowInfo($_GET['book_id'], '1');
 					} else if (!is_numeric($_GET['page'])) {
 						echo '400 Not Valid Parameter';
 					} else {
 						ctr_viewer::ShowImage($_GET['book_id'], $_GET['page']);
+						ctr_viewer::ShowInfo($_GET['book_id'], $_GET['page']);
 					}
 				}
 			?>
