6c30b79aba63829fcb9df4a53103f0dc897dff28
Dictor <kimdictor@gmail.com>
2019-06-21 06:36:12 +0000

    list에서 viewer로 이동 로직 구현

--- controller/ctr_list.php
+++ controller/ctr_list.php
@@ -11,7 +11,7 @@
 		public static function DisplayBooks(array $books) {
 			echo "총 ".count($books)."개의 결과";
 			foreach($books as $nowbook) {
-				print '<a href="#" class="list-group-item list-group-item-action">'.$nowbook->name.'</a>';
+				print '<a href="javascript:go_viewer('.$nowbook->id.')" class="list-group-item list-group-item-action">'.$nowbook->name.'</a>';
 			}
 		}
 	}
