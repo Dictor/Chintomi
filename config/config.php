9c261e96883bd08034864fed064cb00088dbcbe7
Dictor <kimdictor@gmail.com>
2019-06-24 10:54:20 +0000

    설정 파일 설명 주석 추가

--- config/config.php
+++ config/config.php
@@ -1,9 +1,21 @@
 <?php
 	class Config {
+		/*
+		[Path Setting]
+		dataPath :	Path of Comicbook directory
+		sqlitePath :	Path of sqlite DB file
+		allowedExt :	Extension of allowed image file
+		*/
 		const dataPath = "/home/ubuntu/environment/Chintomi/data";
 		const sqlitePath = "/home/ubuntu/environment/Chintomi/model/chintomi.db";
 		const allowedExt = array("jpg", "jpeg", "png", "bmp", "gif");
-		const notFoundimgPath = "/home/ubuntu/environment/Chintomi/config/404.jpg";
+		
+		/*
+		[Image Resize Setting]
+		resizeImage :	TRUE = Enable resizing when image's longer length is over resizeImageSize
+						FALSE = Disable resizing
+		resizeImageSize :	Set threshold size of resizing 
+		*/
 		const resizeImage = TRUE;
 		const resizeImageSize = 1200;
 	}
