b119ebdfa602eda14b4c9ee27761897a88f699f2
Dictor <kimdictor@gmail.com>
2019-06-25 10:48:52 +0000

    book 모델이 업데이트 된 db핸들러에 맞추어 수정되었습니다.

--- model/mdl_book.php
+++ model/mdl_book.php
@@ -4,59 +4,7 @@
 	TABLE comicbook(book_id INTEGER PRIMARY KEY AUTOINCREMENT, book_path TEXT NOT NULL, book_name TEXT, book_author TEXT)  
 	*/
 	require_once 'config/config.php';
-
-	class mdl_book {
-		private static $currentDB = NULL;
-		
-		public static function InitSqlite() {
-			if(!is_file(Config::sqlitePath)){
-				$db = new SQLite3(Config::sqlitePath);
-				$db->exec('CREATE TABLE comicbook(book_id INTEGER PRIMARY KEY AUTOINCREMENT, book_path TEXT NOT NULL, book_name TEXT, book_author TEXT);');
-				$db->close();
-			}
-			
-			$db = new SQLite3(Config::sqlitePath);
-			self::$currentDB = $db;
-			if($db->lastErrorCode() != 0){
-				echo "DB Error : ".$db->lastErrorCode();
-				return FALSE;
-			} else {
-				return TRUE;
-			}
-		}
-		
-		public static function CloseSqlite() {
-			self::$currentDB->close();
-		}
-		
-		public static function GetAllBooks() {
-			return self::QueryResultToArray(self::$currentDB->query('SELECT * FROM comicbook'));
-		}
-		
-		public static function AddBook(Comicbook $book) {
-			self::$currentDB->exec('INSERT INTO comicbook (book_path, book_name, book_author) VALUES ("'.$book->path.'","'.$book->name.'","'.$book->author.'");');
-		}
-		
-		public static function DeleteBook(Comicbook $book) {
-			self::$currentDB->exec('DELETE FROM comicbook WHERE book_id="'.$book->$id.'";');
-		}
-		
-		public static function DeleteBookByPath(string $path) {
-			self::$currentDB->exec('DELETE FROM comicbook WHERE name="'.$path.'";');
-		}
-		
-		public static function SearchBook(string $id) {
-			return self::QueryResultToArray(self::$currentDB->query('SELECT * FROM comicbook WHERE book_id='.$id));
-		}
-		
-		private static function QueryResultToArray($qres) {
-			$res = array();
-			while ($nowrow = $qres->fetchArray()) {
-				$res[]=new Comicbook($nowrow['book_id'], $nowrow['book_path'], $nowrow['book_name'], $nowrow['book_author']);
-			}
-			return $res;
-		}
-	}
+	require_once 'adapter/DBhandler.php';
 
 	class Comicbook {
 		public $name, $path, $id, $author;
@@ -65,6 +13,28 @@
 			$this->path = $path;
 			$this->id = $id;
 			$this->author = $author;
+		}
+	}
+
+	class mdl_book {
+		public static function GetAllBooks() {
+			return hndSQLite::QueryResultToComicbook(hndSQLite::Query('SELECT * FROM comicbook', array()));
+		}
+		
+		public static function AddBook(Comicbook $book) {
+			hndSQLite::Execute('INSERT INTO comicbook (book_path, book_name, book_author) VALUES (?, ?, ?)', array($book->path, $book->name, $book->author));
+		}
+		
+		public static function DeleteBook(Comicbook $book) {
+			hndSQLite::Execute('DELETE FROM comicbook WHERE book_id=?', array($book->$id));
+		}
+		
+		public static function DeleteBookByPath(string $path) {
+			hndSQLite::Execute('DELETE FROM comicbook WHERE name=?', array($path));
+		}
+		
+		public static function SearchBook(string $id) {
+			return hndSQLite::QueryResultToComicbook(hndSQLite::Query('SELECT * FROM comicbook WHERE book_id=?', array($id)));
 		}
 	}
 ?>
