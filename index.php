937ff8f8b7e7282957d9ba07cc8cc8116188e1e9
Dictor <kimdictor@gmail.com>
2019-08-18 09:18:34 +0000

    아이콘 정상적으로 표시되게 스타일시트 임포트

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 		
 		<link href="/style/standard.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css" rel="stylesheet" type="text/css">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css" rel="stylesheet">
 		
 		<!--BootStrap-->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
