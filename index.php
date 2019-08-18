484297f3498dda8fdb669ed1793b26775f642b06
Dictor <kimdictor@gmail.com>
2019-08-18 08:40:00 +0000

    인덴트 수정 (close #15)

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		  <title>Chintomi</title>
+		<title>Chintomi</title>
 		
 		<link href="/style/standard.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/earlyaccess/nanumgothic.css" rel="stylesheet" type="text/css">
