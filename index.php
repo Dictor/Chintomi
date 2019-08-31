a02b4d9d6c0a0f680d7f2185f7558a46ab4fae36
Dictor <kimdictor@gmail.com>
2019-08-31 09:00:47 +0000

    index.php도 세션 기능 활성화

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php 
 	namespace Dictor\Chintomi; 
 	require_once 'autoload.php';
-?>
+    
+    session_start();
+	$urlarg = filter_var($_GET['path'], FILTER_SANITIZE_STRING);
+	$urlarg = trim($urlarg, '/');
+	$urlargs = explode('/', $urlarg);
+	if ($urlargs[0] === 'action') {
+	    ctr_api::Process($urlargs);
+	} else {
+	    echo 
+<<<HTMLSTART
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -18,29 +27,27 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</head>
-<?php
-	$urlarg = filter_var($_GET['path'], FILTER_SANITIZE_STRING);
-	$urlarg = trim($urlarg, '/');
-	$urlargs = explode('/', $urlarg);
-    switch ($urlargs[0]) {
-        case 'index':
-            require 'view/view_index.php';
-            break;
-        case 'list':
-            require 'view/view_list.php';
-            break;
-        case 'viewer':
-            require 'view/view_viewer.php';
-            break;
-        case 'setup':
-            require 'view/view_setup.php';
-            break;
-        case 'setting':
-            require 'view/view_setting.php';
-            break;
-        default:
-            require 'view/view_index.php';
-            break;
-    }
+HTMLSTART;
+	    switch ($urlargs[0]) {
+            case 'index':
+                require 'view/view_index.php';
+                break;
+            case 'list':
+                require 'view/view_list.php';
+                break;
+            case 'viewer':
+                require 'view/view_viewer.php';
+                break;
+            case 'setup':
+                require 'view/view_setup.php';
+                break;
+            case 'setting':
+                require 'view/view_setting.php';
+                break;
+            default:
+                require 'view/view_index.php';
+                break;
+        }
+	}
 ?>
 </html>
