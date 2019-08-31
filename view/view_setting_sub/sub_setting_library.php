5a2947a71e4f6e5bb271eb7c7d185f1cf9274ab6
Dictor <kimdictor@gmail.com>
2019-08-31 09:02:42 +0000

    rest api 기능 로그아웃, 라이브러리 모두 업데이트 추가

---
+++ view/view_setting_sub/sub_setting_library.php
@@ -0,0 +1,10 @@
+<?php 
+	namespace Dictor\Chintomi; 
+	require_once 'autoload.php';
+?>
+<div class='padding-div'>
+    <button type="button" class="btn btn-primary btn-lg btn-block">라이브러리 업데이트</button>
+    <p>'라이브러리 업데이트'는 아래 두 버튼의 작업을 한꺼번에 실행합니다.</p>
+    <button type="button" class="btn btn-secondary btn-lg btn-block">만화책 라이브러리 업데이트</button>
+    <button type="button" class="btn btn-secondary btn-lg btn-block">썸네일 업데이트</button>
+</div>
