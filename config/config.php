79d58df7bb4ebb05f01d129b7286c1d4a71950f2
Dictor <kimdictor@gmail.com>
2019-08-31 08:09:52 +0000

    setting 페이지에서 환경 설정을 표출하기 위해 config 클래스에 상수 멤버 표출 함수 추가

--- config/config.php
+++ config/config.php
@@ -11,28 +11,28 @@
 		const PATH_COMICBOOK = "/home/ubuntu/environment/Chintomi/data";
 		const PATH_SQLITE = "/home/ubuntu/environment/Chintomi/data/chintomi.db";
 		const ALLOWED_EXTENSION = array("jpg", "jpeg", "png", "bmp", "gif");
-		
+
 		/*
 		[IMAGE RESIZE SETTING]
 		RESIZEIMG_ENABLE	: TRUE = Enable resizing when image's longer length is over resizeImageSize
 							  FALSE = Disable resizing
 							  (Resizing image expends TTFB due to resizing process time. So, enable it when necessary situation like low-bandwidth network.)
-		RESIZEIMG_THRESHOLD : Set threshold size of resizing 
+		RESIZEIMG_THRESHOLD : Set threshold size of resizing
 		*/
 		const RESIZEIMG_ENABLE = FALSE;
 		const RESIZEIMG_THRESHOLD = 1200;
-		
+
 		/*
 		[PERMISSION SETTING]
 		- User who has more than each level input can access each function.
-		PERMISSION_LEVEL_ADMIN	: Permission of super admin who can access admin page. 
+		PERMISSION_LEVEL_ADMIN	: Permission of super admin who can access admin page.
 		PERMISSION_LEVEL_VIEWER	: Permission of user who can access viewer page.
-		PERMISSION_LEVER_LIST	: Permission of user who can access list page. 
+		PERMISSION_LEVER_LIST	: Permission of user who can access list page.
 		*/
 		const PERMISSION_LEVEL_ADMIN = 999;
 		const PERMISSION_LEVEL_VIEWER = 2;
 		const PERMISSION_LEVEL_LIST = 2;
-		
+
 		/*
 		[INPUT VALIDATION SETTING]
 		INPUT_VALIDATION_USERNAME	: Regular expression used for user name string validation
@@ -40,7 +40,7 @@
 		*/
 		const INPUT_VALIDATION_USERNAME = '/^[A-Za-z0-9]{4,12}$/';
 		const INPUT_VALIDATION_PASSWORD = '/^.*(?=^.{4,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/';
-		
+
 		/*
 		[LIST PAGINATION SETTING]
 		LIST_PAGIGATION_ENABLE		: When this is true, pagination function in 'list.php' is enabled.
@@ -48,7 +48,7 @@
 		*/
 		const LIST_PAGIGATION_ENABLE = TRUE;
 		const LIST_PAGIGATION_THRESHOLD = 15;
-		
+
 		/*
 		[THUMBNAIL SETTING]
 		THUMBNAIL_DISPLAY_ENABLE	: When this is true, thumbnail will display on list page
@@ -58,12 +58,19 @@
 		const THUMBNAIL_DISPLAY_ENABLE = TRUE;
 		const THUMBNAIL_LONGSIDE_LENGTH = 150;
 		const THUMBNAIL_QUALITY = 85;
-		
+
 		/*
 		[URL REWRITE SETTING]
 		URLREWRITE_ENABLE	: TRUE = (Only for URL Rewrite enabled WAS) URL is formatted shortly and beautiful (It require URL rewrite through rule defined in .htaccess)
 							  FALSE	= (for URL Rewrite disabled or not supported WAS) URL isn't formatted shortly
 		*/
 		const URLREWRITE_ENABLE = FALSE;
+
+		public static function GetMember() {
+			$refl = new \ReflectionClass(get_called_class());
+			return $refl->getConstants();
+		}
 	}
+
+
 ?>
