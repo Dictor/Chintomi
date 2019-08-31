63cd63ea994f547e2decd06b9847fd904953024b
Dictor <kimdictor@gmail.com>
2019-08-31 09:01:17 +0000

    라이브러리, 썸네일 업데이트 함수에 결과값 반환 추가

--- model/mdl_library.php
+++ model/mdl_library.php
@@ -12,21 +12,27 @@
 			$validpaths = self::ExploreDirectory();
 			$dbbooks = mdl_book::GetAllBooks();
 			$dbpaths = array();
+			$res = array('added' => 0, 'deleted' => 0);
+			
 			foreach($dbbooks as $nowbook) {
 				$dbpaths[] = $nowbook->path;
 			}
 			foreach(array_diff($validpaths, $dbpaths) as $nowadd) {
 				$name = explode('/',$nowadd);
 				mdl_book::AddBook(new Comicbook(NULL, $nowadd, end($name), ""));
+				$res['added']++;
 			}
 			foreach(array_diff($dbpaths, $validpaths) as $nowdel) {
 				mdl_book::DeleteBookByPath($nowdel);
+				$res['deleted']++;
 			}
+			return $res;
 		}
 		
 		public static function UpdateThumbnail() {
 			if (!is_dir(self::$thumbdir)) mkdir(self::$thumbdir);
 			$dbbooks = mdl_book::GetAllBooks();
+			$res = 0;
 			foreach($dbbooks as $nowbook) {
 				$thumbsrc = ctr_viewer::GetImagePath(mdl_library::GetEntry($nowbook->path), 1);
 				if(!is_file(self::$thumbdir.'/'.(string)$nowbook->id.'.jpg')) {
@@ -34,8 +40,10 @@
 					$image->quality_jpg = config::THUMBNAIL_QUALITY;
 					$image->resizeToLongSide(config::THUMBNAIL_LONGSIDE_LENGTH);
 					$image->save(self::$thumbdir.'/'.(string)$nowbook->id.'.jpg', IMAGETYPE_JPEG);
+					$res++;
 				}
 			}
+			return $res;
 		}
 		
 		private static function ExploreDirectory() {
