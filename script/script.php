334f2c9ab44e7c17d2209e69917f3be78a93f98c
Dictor <kimdictor@gmail.com>
2019-09-12 10:40:20 +0000

    script 디렉터리를 추가하고 js를 제공하는 script.php 구현

---
+++ script/script.php
@@ -0,0 +1,38 @@
+<?php 
+	namespace Dictor\Chintomi; 
+	require_once 'autoload.php';
+	if (!mdl_user::CheckPermission(0)) {
+	    echo '/*NO AUTHORITY*/';
+	    exit();
+	}
+	echo 'var api_host = "'.utl_htmldoc::GetHrefPath('PAGE_API').'";';
+?>
+
+function GETApiReq(verb, okcb, errorcb) {
+    var req = new XMLHttpRequest();
+    req.open("GET", api_host + "/" + verb, true);
+    req.onload = function() {
+        if (req.status == 200) {
+            okcb();
+        } else {
+            errorcb();
+        }
+    };
+    req.onerror = errorcb();
+    req.send();
+}
+
+function POSTApiReq(verb, param, okcb, errorcb) {
+    var req = new XMLHttpRequest();
+    req.open("POST", api_host + "/" + verb, true);
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.onload = function() {
+        if (req.status == 200) {
+            okcb();
+        } else {
+            errorcb();
+        }
+    };
+    req.onerror = errorcb();
+    req.send(param);
+}
