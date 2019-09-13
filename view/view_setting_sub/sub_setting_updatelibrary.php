2046cd1f5635e204d5b597beb20c0ae05aa1c68e
Dictor <kimdictor@gmail.com>
2019-09-13 06:51:33 +0000

    새로 추가된 공통 js의 ApiReq 함수를 이용하게 구현 수정

--- view/view_setting_sub/sub_setting_updatelibrary.php
+++ view/view_setting_sub/sub_setting_updatelibrary.php
@@ -1,7 +1,7 @@
 <?php 
 	namespace Dictor\Chintomi; 
 	require_once 'autoload.php';
-	echo '<script>var api_host = "'.utl_htmldoc::GetHrefPath('PAGE_API').'";</script>';
+	echo '<script src="'.utl_htmldoc::GetHrefPath('PAGE_JS').'"></script>';
 ?>
 <script>
     function library_update(kind) {
@@ -24,30 +24,19 @@
                 thnext_task();
                 return;
         }
-        var req = new XMLHttpRequest();
-        req.open("GET", api_host + "/" + api_path, true);
-        req.onload = function() {
-            if (req.status == 200) {
-                var res = JSON.parse(req.response);
+        GETApiReq(api_path, function(resp) { /*global GETApiReq*/
+                var res = JSON.parse(resp);
                 printres("작업 완료! 결과 : " + (res['res'] == 'success' ? "성공" : "실패"));
                 printres("메세지 : " + res['msg']);
                 printres("만화책 추가 : " + res['lib_added'] + ", 만화책 제거 : " + res['lib_deleted'] + ", 썸네일 추가 : " + res['th_added']);
-            } else {
-                printres("작업 중 오류가 발생했습니다! : 응답 " + req.status);
-            }
-        };
-        req.onerror = function() {
-            printres("작업 중 오류가 발생했습니다!");
-        }
-        req.send();
+            }, function() {
+                printres("작업 중 오류가 발생했습니다!");
+            });
     }
     
     function thnext_task() {
-        var req = new XMLHttpRequest();
-        req.open("GET", api_host + "/update_th_next", true);
-        req.onload = function() {
-            if (req.status == 200) {
-                var res = JSON.parse(req.response);
+        GETApiReq("update_th_next", function(resp) {
+                var res = JSON.parse(resp);
                 printres("작업 완료! 결과 : " + (res['res'] == 'success' ? "성공" : "실패"));
                 printres("메세지 : " + res['msg'] + " 썸네일 추가 : " + res['th_added']);
                 if (res['th_added'] != "id → -1") {
@@ -55,19 +44,13 @@
                 } else {
                     printres("순차 작업을 종료합니다!");
                 }
-            } else {
-                printres("작업 중 오류가 발생했습니다! : 응답 " + req.status);
-            }
-        };
-        req.onerror = function() {
-            printres("작업 중 오류가 발생했습니다!");
-        }
-        req.send();
-        
+            }, function() {
+                printres("작업 중 오류가 발생했습니다!");
+            })
     }
     
     function printres(txt) {
-        $("#result").html($("#result").html() + "<br>→ " + txt);
+        $("#result").html($("#result").html() + "<br>→ " + txt); /* global $ */
         $("#result").scrollTop($("#result")[0].scrollHeight);
     }
 </script>
