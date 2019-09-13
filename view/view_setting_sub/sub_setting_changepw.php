666d5496aae890f5c7d6d9e2795dbbbb652c4767
Dictor <kimdictor@gmail.com>
2019-09-13 07:56:12 +0000

    git change-pw 서브 페이지 기능 구현

--- view/view_setting_sub/sub_setting_changepw.php
+++ view/view_setting_sub/sub_setting_changepw.php
@@ -1,14 +1,31 @@
 <?php 
 	namespace Dictor\Chintomi; 
 	require_once 'autoload.php';
-	echo '<script>var api_host = "'.utl_htmldoc::GetHrefPath('PAGE_API').'";</script>';
+	echo '<script src="'.utl_htmldoc::GetHrefPath('PAGE_JS').'"></script>';
 ?>
 <script>
     function change() {
-        if ($("#newPw").val() != $("#newPwChk").val()) {
+        if ($("#newPw").val() != $("#newPwChk").val()) { /* global $ */
             alert("새 비밀번호와 새 비밀번호 확인이 일치하지 않습니다!");
         } else {
-            
+            var passform = new FormData();
+            passform.append("nowpass", $("#nowPw").val());
+            passform.append("newpass", $("#newPw").val());
+            POSTApiReq("change_pw", passform, function(resp) {
+                var res = JSON.parse(resp);
+                if (res["res"] == "success") {
+                    alert("비밀번호를 성공적으로 변경했습니다!");
+                } else {
+                    switch(res["msg"]) {
+                        case "now pw incorrect": alert("현재 비밀번호가 올바르지 않습니다!"); break;
+                        case "input invalid": alert("비밀번호가 규칙에 맞지 않습니다!"); break;
+                        case "invalid params": alert("매개변수 오류"); break;
+                        default: alert("알 수 없는 오류"); break;
+                    }
+                }
+            }, function() {
+                //alert("작업도중 오류가 발생했습니다!");
+            });
         }
     }
 </script>
