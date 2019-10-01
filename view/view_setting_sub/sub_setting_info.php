e761feb4e61a9364cdc55ab8db2fc2db73ccbb43
Dictor <kimdictor@gmail.com>
2019-10-01 06:33:43 +0000

    정보 페이지 버전 업데이트 반영

--- view/view_setting_sub/sub_setting_info.php
+++ view/view_setting_sub/sub_setting_info.php
@@ -5,7 +5,7 @@
 ?>
 <div class='padding-div'>
     <h1>Chintomi</h1>
-    <h4>버전 : alpha6-p2 (20190907)</h4>
+    <h4>버전 : alpha6-p4 (20191001)</h4>
     <h4>제작 : Dictor (kimdictor@gmail.com)</h4>
     <h4><a href="https://wiki.chinchister.com/chintomi">사용법</a>, <a href="https://github.com/Dictor/Chintomi">업데이트 확인</a></h4>
 </div>
