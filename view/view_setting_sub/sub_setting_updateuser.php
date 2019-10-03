4833ba9463f72b254bd6abbbd51dbab47c7130b5
Dictor <kimdictor@gmail.com>
2019-10-03 04:58:24 +0000

    사용자 생성 UI 개선

--- view/view_setting_sub/sub_setting_updateuser.php
+++ view/view_setting_sub/sub_setting_updateuser.php
@@ -27,29 +27,37 @@
     }
     
     function makeusr() {
-            var pform = new FormData();
-            pform.append("uname", prompt("유저 ID?"));
-            pform.append("upass", prompt("유저 PW?"));
-            pform.append("uper", prompt("유저 권한 레벨?"));
-            POSTApiReq("make_user", pform, function(resp) {
-                    var res = JSON.parse(resp);
-                    if (res["res"] == "success") {
-                        alert("계정을 성공적으로 생성했습니다");
-                        location.reload();
-                    } else {
-                        switch(res["msg"]) {
-                            case "input invalid": alert("유효하지 않은 입력입니다!"); break;
-                            case "already exist user": alert("이미 존재하는 유저입니다!"); break;
-                            case "invalid params": alert("매개변수 오류"); break;
-                            case "no authority": alert("권한 없음"); break;
-                            default: alert("알 수 없는 오류"); break;
+            if ($("#add-upass").val() != $("#add-upasschk").val()) { /* global $ */
+                alert("새 비밀번호와 새 비밀번호 확인이 일치하지 않습니다!");
+            } else {
+                var pform = new FormData();
+                pform.append("uname", $("#add-uname").val());
+                pform.append("upass", $("#add-upass").val());
+                pform.append("uper", $("#add-uper").val());
+                POSTApiReq("make_user", pform, function(resp) {
+                        var res = JSON.parse(resp);
+                        if (res["res"] == "success") {
+                            alert("계정을 성공적으로 생성했습니다");
+                            location.reload();
+                        } else {
+                            switch(res["msg"]) {
+                                case "input invalid": alert("조건에 맞지 않는 유효하지 않은 입력입니다!"); break;
+                                case "already exist user": alert("이미 존재하는 유저입니다!"); break;
+                                case "invalid params": alert("매개변수 오류"); break;
+                                case "no authority": alert("권한 없음"); break;
+                                default: alert("알 수 없는 오류"); break;
+                            }
                         }
-                    }
                 }, function() {}); 
+            }
     }
 </script>
 <div class='padding-div'>
-    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="javascript:makeusr()">사용자 추가</button>
+    <input type="text" required class="form-control half-size-input" id="add-uname" placeholder="사용자 ID">
+    <input type="number" required min=0 max=999 class="form-control half-size-input" id="add-uper" placeholder="사용자 권한">
+    <input type="password" required class="form-control" id="add-upass" placeholder="새 비밀번호">
+    <input type="password" required class="form-control" id="add-upasschk" placeholder="새 비밀번호 확인">
+    <button type="button" required class="btn btn-primary btn-lg btn-block" onclick="javascript:makeusr()">사용자 추가</button>
 </div>
 <table class="table table-hover">
     <thead>
