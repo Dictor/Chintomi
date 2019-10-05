188c967338889850bb62b389159543655dcfd66d
Dictor <kimdictor@gmail.com>
2019-10-05 06:30:45 +0000

    라이브러리 업데이트 서브페이지에 도움말 추가

--- view/view_setting_sub/sub_setting_updatelibrary.php
+++ view/view_setting_sub/sub_setting_updatelibrary.php
@@ -5,9 +5,10 @@
 ?>
 <div class='padding-div'>
     <button type="button" class="btn btn-primary btn-lg btn-block" onclick="javascript:spSetting.apiUpdateLibrary(0)">라이브러리 업데이트</button>
-    <p>'라이브러리 업데이트'는 아래 두 버튼의 작업을 한꺼번에 실행합니다.</p>
+    <p>'라이브러리 업데이트'는 '만화책 라이브러리 업데이트'와 '썸네일 일괄 업데이트' 작업을 한번에 실행합니다.</p>
     <button type="button" class="btn btn-secondary btn-lg btn-block" onclick="javascript:spSetting.apiUpdateLibrary(1)">만화책 라이브러리 업데이트</button>
     <button type="button" class="btn btn-secondary btn-lg btn-block" onclick="javascript:spSetting.apiUpdateLibrary(2)">썸네일 일괄 업데이트</button>
+    <p>'썸네일 순차 업데이트'는 많은 만화책의 썸네일을 한번에 생성할 때 권장됩니다.</p>
     <button type="button" class="btn btn-secondary btn-lg btn-block" onclick="javascript:spSetting.apiUpdateLibrary(3)">썸네일 순차 업데이트</button>
     <div id='result'></div>
 </div>
