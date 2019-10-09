20efec1b3dd5c657c7fdd8e62b3902cb2c1e1523
Dictor <kimdictor@gmail.com>
2019-10-09 02:20:10 +0000

    환경 설정 확인과 라이브러리 확인 서브페이지의 표를 모바일을 위해 반응형으로 수정

--- view/view_setting_sub/sub_setting_viewconfig.php
+++ view/view_setting_sub/sub_setting_viewconfig.php
@@ -2,14 +2,16 @@
 	namespace Dictor\Chintomi; 
 	require_once 'autoload.php';
 ?>
-<table class="table table-hover">
-    <thead>
-        <tr>
-          <th scope="col">변수 이름</th>
-          <th scope="col">변수 값</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php ctr_setting::spDisplayConfig() ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+              <th scope="col">변수 이름</th>
+              <th scope="col">변수 값</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php ctr_setting::spDisplayConfig() ?>
+        </tbody>
+    </table>
+</div>
--- view/view_setting_sub/sub_setting_viewlibrary.php
+++ view/view_setting_sub/sub_setting_viewlibrary.php
@@ -2,16 +2,18 @@
 	namespace Dictor\Chintomi; 
 	require_once 'autoload.php';
 ?>
-<table class="table table-hover">
-    <thead>
-        <tr>
-          <th scope="col">책 ID</th>
-          <th scope="col">책 이름</th>
-          <th scope="col">책 경로</th>
-          <th scope="col">책 저자</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php ctr_setting::spDisplayLibrary() ?>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+              <th scope="col">책 ID</th>
+              <th scope="col">책 이름</th>
+              <th scope="col">책 경로</th>
+              <th scope="col">책 저자</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php ctr_setting::spDisplayLibrary() ?>
+        </tbody>
+    </table>
+</div>
