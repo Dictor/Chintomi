d5b046cb12579dfd85aab34b9e5351359d0542ec
Dictor <kimdictor@gmail.com>
2019-12-22 07:35:05 +0000

    DB 핸들러 개선 및 statement 클래스 개선
    * DB 핸들러 인터페이스에 함수 매개변수 및 반환값 강제하게 수정
    * statement 클래스에 함수 매개변수 및 반환값 강제하게 수정

--- util/DBhandler.php
+++ util/DBhandler.php
@@ -2,86 +2,78 @@
 	namespace Dictor\Chintomi;
 	
     interface handler {
-		public static function Open($path);
-		public static function Close();
-		public static function Execute(string $preQuery, array $parameter);
-		public static function Query(string $preQuery, array $parameter);
-		public static function ResultToArray($res);
-		public static function ResultToComicbook($res);
+		public static function Open($path): int;
+		public static function Close(): void;
+		public static function Execute(string $preQuery, array $parameter): \SQLite3Result;
+		public static function Query(string $preQuery, array $parameter): \SQLite3Result;
+		public static function ResultToArray($res): array;
+		public static function ResultToComicbook($res): array;
 	}
 	
 	class hndStatement {
 		const TYPE_EQUAL = 0;
 		const TYPE_LIKE = 1;
 		
-		const HANDLER_SQLITE = 0;
-		const HANDLER_JSON = 1;
-		
 		private $params = array();
 		private $where_clause_count = 0;
 		
-		public function select() {
+		public function select(): hndStatement {
 			$this->params['action'] = 'select';
 			return $this;
 		}
 		
-		public function insert(array $data) {
+		public function insert(array $data): hndStatement {
 			$this->params['action'] = 'insert';
 			$this->params['insert_data'] = $data;
 			return $this;
 		}
 		
-		public function update(string $column_name, string $value) {
+		public function update(string $column_name, string $value): hndStatement {
 			$this->params['action'] = 'update';
 			$this->params["update_col_name"] = $column_name;
 			$this->params["update_col_val"] = $value;
 			return $this;
 		}
 		
-		public function delete() {
+		public function delete(): hndStatement {
 			$this->params['action'] = 'delete';
 			return $this;
 		}
 		
-		public function table(string $table_name) {
+		public function table(string $table_name): hndStatement {
 			$this->params['table'] = $table_name;
 			return $this;
 		}
 		
-		public function where(int $clause_type, string $column_name, string $value, string $merge = 'AND') {
+		public function where(int $clause_type, string $column_name, string $value, string $merge = 'AND'): hndStatement {
 			$this->params[$this->where_clause_count] = [$clause_type, $column_name, $value, $merge];
 			$this->where_clause_count++;
 			return $this;
 		}
 		
-		public function toRealStatement(int $db_type) {
-			switch($db_type) {
-				case self::HANDLER_JSON:
-					return $this->params;
-				case self::HANDLER_SQLITE:
-					$rtn_string = '';
-					switch($this->params['action']) {
-						case 'insert':
-							$arr = self::toArrayExpr($this->params['insert_data']);
-							$rtn_string .= 'INSERT INTO '.$this->params['table'].' ('.$arr['col'].') VALUES ('.$arr['val'].')';
-							break;
-						case 'select':
-							$rtn_string .= 'SELECT * FROM '.$this->params['table'];
-							if ($this->where_clause_count > 0) $rtn_string .= ' WHERE '.$this->makeWhereStmt();
-							break;
-						case 'update':
-							/* Update statement unused in chintomi currently */
-							break;
-						case 'delete':
-							$rtn_string .= 'DELETE * FROM '.$this->params['table'];
-							if ($this->where_clause_count > 0) $rtn_string .= ' WHERE '.$this->makeWhereStmt();
-							break;
-					}
-					return $rtn_string;
+		public function toSqlStatement(): string {
+			$rtn_string = '';
+			switch($this->params['action']) {
+				case 'insert':
+					$arr = self::toSqlArrayExpr($this->params['insert_data']);
+					$rtn_string .= 'INSERT INTO '.$this->params['table'].' ('.$arr['col'].') VALUES ('.$arr['val'].')';
+					break;
+				case 'select':
+					$rtn_string .= 'SELECT * FROM '.$this->params['table'];
+					if ($this->where_clause_count > 0) $rtn_string .= ' WHERE '.$this->makeWhereStmt();
+					break;
+				case 'update':
+					/* Update statement unused in chintomi currently */
+					break;
+				case 'delete':
+					$rtn_string .= 'DELETE * FROM '.$this->params['table'];
+					if ($this->where_clause_count > 0) $rtn_string .= ' WHERE '.$this->makeWhereStmt();
+					break;
 			}
+			return $rtn_string;
 		}
 		
-		private static function toArrayExpr(array $arr) {
+		private static function toSqlArrayExpr(array $arr): array {
 			$value_col = '';
 			$value_val = '';
 			foreach ($arr as $col => $val) {
@@ -93,11 +85,11 @@
 			return ['col' => $value_col, 'val' => $value_val];
 		}
 		
-		private static function toSqlExpr($var) {
+		private static function toSqlExpr($var): string {
 			return (gettype($var) == "string" ? "'".$var."'" : strval($var));
 		}
 		
-		private function makeWhereStmt() {
+		private function makeWhereStmt(): string {
 			$condition = ''; 
 			for ($i = 0; $i < $this->where_clause_count; $i++) {
 				$condition .= ' '.$this->params[$i][3].' '.$this->params[$i][1];
@@ -113,7 +105,7 @@
 		private static $currentDB;
 		private static $isOpen = FALSE;
 		
-		public static function Open($path) {
+		public static function Open($path): int {
 			try{
 				if(!is_dir(dirname($path))) mkdir(dirname($path), 0777, TRUE);
 				if(!is_file($path)){
@@ -136,15 +128,15 @@
 			}
 		}
 		
-		public static function Close() {
+		public static function Close(): void {
 			self::$currentDB->close();
 		}
 		
-		public static function Execute(string $preQuery, array $parameter) {
+		public static function Execute(string $preQuery, array $parameter): \SQLite3Result {
 			return self::Query($preQuery, $parameter);
 		}
 		
-		public static function Query(string $preQuery, array $parameter) {
+		public static function Query(string $preQuery, array $parameter): \SQLite3Result {
 			$state = self::$currentDB->prepare($preQuery);
 			if($state == FALSE) return FALSE;
 			foreach($parameter as $nowkey => $nowval) {
@@ -153,7 +145,7 @@
 			return $state->execute();
 		}
 		
-		public static function ResultToArray($res) {
+		public static function ResultToArray($res): array {
 			$arr = array();
 			while ($nowrow = $res->fetchArray(SQLITE3_ASSOC)){
 				if($nowrow != FALSE) $arr[] = $nowrow;
@@ -161,7 +153,7 @@
 			return $arr;
 		}
 		
-		public static function ResultToComicbook($res) {
+		public static function ResultToComicbook($res): array {
 			$arr = array();
 			while ($nowrow = $res->fetchArray()) {
 				$arr[]=new Comicbook($nowrow['book_id'], $nowrow['book_path'], $nowrow['book_name'], $nowrow['book_author'], $nowrow['image_count'], $nowrow['image_size'], $nowrow['added_date']);
