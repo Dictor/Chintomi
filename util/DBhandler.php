1d263493242b2cf8fb72227fbd6b725e317214c1
Dictor <kimdictor@gmail.com>
2019-12-23 11:07:28 +0000

    db 핸들러의 실행, 쿼리 함수의 반환값 명시는 의도에 맞지않아 삭제

--- util/DBhandler.php
+++ util/DBhandler.php
@@ -4,8 +4,8 @@
     interface handler {
 		public static function Open($path): int;
 		public static function Close(): void;
-		public static function Execute(string $preQuery, array $parameter): \SQLite3Result;
-		public static function Query(string $preQuery, array $parameter): \SQLite3Result;
+		public static function Execute(string $preQuery, array $parameter);
+		public static function Query(string $preQuery, array $parameter);
 		public static function ResultToArray($res): array;
 		public static function ResultToComicbook($res): array;
 	}
