4169f40969a4e086b36902a168712f9837d52cd3
Dictor <kimdictor@gmail.com>
2019-12-25 07:30:37 +0000

    임시로 Execute, Query 메서드를 인터페이스 구현에서 제회
    * 두 메서드가 SQL 문자열 쿼리에만 종속적임

--- handler/handler.php
+++ handler/handler.php
@@ -4,8 +4,13 @@ namespace Dictor\Chintomi;
 interface handler {
 	public static function Open($path): int;
 	public static function Close(): void;
+	
+	/*
+	이 두개의 메서드가 핸들러 인터페이스 구현에 필수적일지는 검토 필요 (함수가 SQL text query에만 종속적임)
 	public static function Execute(string $preQuery, array $parameter);
 	public static function Query(string $preQuery, array $parameter);
+	*/
+	
 	public static function ResultToArray($res): array;
 	public static function ResultToComicbook($res): array;
 }
