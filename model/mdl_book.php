42c9d18bc4e17bc0d94e23a85ee176b500a02140
Dictor <kimdictor@gmail.com>
2019-12-25 04:57:48 +0000

    다중 db 핸들러 변경에 대응하게 모델 수정
    * 모든 모델 함수 반환 값 형 명시하게 수정

--- model/mdl_book.php
+++ model/mdl_book.php
@@ -16,36 +16,52 @@
 	}
 
 	class mdl_book {
-		public static function UseDB() {
-            return hnd_SQLite::Open(config::PATH_SQLITE);
+		public static function UseDB(): int {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': return hnd_SQLite::Open(config::PATH_SQLITE);
+			}
 		}
 		
-		public static function GetAllBooks() {
-			return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook', array()));
+		public static function GetAllBooks(): array {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook', array()));
+			}
 		}
 		
-		public static function AddBook(Comicbook $book) {
-			return hnd_SQLite::Execute('INSERT INTO comicbook (book_path, book_name, book_author, image_count, image_size, added_date) VALUES (:bpath, :bname, :bauthor, :bimgcnt, :bimgsize, :bdate)', 
-				array('bpath' => $book->path, 'bname' => $book->name, 'bauthor' => $book->author, 'bimgcnt' => $book->imgcnt, 'bimgsize' => $book->imgsize, 'bdate' => (new \DateTime('now')) -> format(\DateTime::ATOM)));
+		public static function AddBook(Comicbook $book): void {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE':
+					hnd_SQLite::Execute('INSERT INTO comicbook (book_path, book_name, book_author, image_count, image_size, added_date) VALUES (:bpath, :bname, :bauthor, :bimgcnt, :bimgsize, :bdate)', 
+						array('bpath' => $book->path, 'bname' => $book->name, 'bauthor' => $book->author, 'bimgcnt' => $book->imgcnt, 'bimgsize' => $book->imgsize, 'bdate' => (new \DateTime('now')) -> format(\DateTime::ATOM)));
+					break;
+			}
 		}
 		
-		public static function DeleteBook(Comicbook $book) {
-			return hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_id=:bid', array('bid' => $book->$id));
+		public static function DeleteBook(Comicbook $book): void {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_id=:bid', array('bid' => $book->$id)); break;
+			}
 		}
 		
-		public static function DeleteAllBooks() {
-			return hnd_SQLite::Execute('DELETE FROM comicbook', array());
+		public static function DeleteAllBooks(): void {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook', array()); break;
+			}
 		}
 		
-		public static function DeleteBookByPath(string $path) {
-			return hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_path=:bpath', array('bpath' => $path));
+		public static function DeleteBookByPath(string $path): void {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_path=:bpath', array('bpath' => $path)); break;
+			}
 		}
 		
-		public static function SearchBook(string $id) {
-			return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook WHERE book_id=:bid', array('bid' => $id)));
+		public static function SearchBook(string $id): array {
+			switch (config::DB_HANDLER) {
+                case 'SQLITE': return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook WHERE book_id=:bid', array('bid' => $id)));
+			}
 		}
 		
-		public static function GetHumanFileSize($bytes, $decimals = 2) {
+		public static function GetHumanFileSize($bytes, $decimals = 2): string {
 		  $sz = 'BKMGTP';
 		  $factor = floor((strlen($bytes) - 1) / 3);
 		  return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
--- model/mdl_user.php
+++ model/mdl_user.php
@@ -3,12 +3,17 @@
     require_once 'autoload.php';
     
     class mdl_user {
-        public static function UseDB() {
-            return hnd_SQLite::Open(config::PATH_SQLITE);
+        public static function UseDB(): int {
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': return hnd_SQLite::Open(config::PATH_SQLITE);
+            }
 		}
         
-        public static function CheckPassword($userName, $userPass) {
-            $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $userName)));
+        public static function CheckPassword($userName, $userPass): bool {
+            $res = [];
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $userName))); break;
+            }
             if (!is_null($res) and count($res) != 0) {
                 if(password_verify($userPass, $res[0]['user_pass'])) {
                     return TRUE;
@@ -20,24 +25,35 @@
             }
         }
         
-        public static function ChangePassword($userName, $newPass) {
-            return hnd_SQLite::Execute('UPDATE user SET user_pass = :newpass WHERE user_name = :uname', array('uname' => $userName, 'newpass' => password_hash($newPass, PASSWORD_DEFAULT)));    
+        public static function ChangePassword($userName, $newPass): void {
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('UPDATE user SET user_pass = :newpass WHERE user_name = :uname', array('uname' => $userName, 'newpass' => password_hash($newPass, PASSWORD_DEFAULT))); break;   
+            }
         }
         
-        public static function ChangePermission($uname, $newper) {
-            return hnd_SQLite::Execute('UPDATE user SET user_permission = :uper WHERE user_name = :uname', array('uname' => $uname, 'uper' => $newper));    
+        public static function ChangePermission($uname, $newper): void {
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('UPDATE user SET user_permission = :uper WHERE user_name = :uname', array('uname' => $uname, 'uper' => $newper)); break;
+            }
         }
         
-        public static function DeleteUser($userName) {
-            return hnd_SQLite::Execute('DELETE FROM user WHERE user_name = :uname', array('uname' => $userName)); 
+        public static function DeleteUser($userName): void {
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('DELETE FROM user WHERE user_name = :uname', array('uname' => $userName)); break;
+            }
         }
         
-        public static function MakeUser($uname, $upass, $uper) {
-            return hnd_SQLite::Execute('INSERT INTO user VALUES (:uname, :upass, :uper);', array('uname' => $uname, 'upass' => password_hash($upass, PASSWORD_DEFAULT), 'uper' => $uper));
+        public static function MakeUser($uname, $upass, $uper): void {
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': hnd_SQLite::Execute('INSERT INTO user VALUES (:uname, :upass, :uper);', array('uname' => $uname, 'upass' => password_hash($upass, PASSWORD_DEFAULT), 'uper' => $uper)); break;
+            }
         }
         
-        public static function GetAllUser() {
-            $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user', array()));
+        public static function GetAllUser(): array {
+            $res = [];
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user', array())); break;
+            }
             $rtn = array();
             foreach ($res as $nowuser) {
                 $rtn[$nowuser['user_name']] = $nowuser['user_permission'];
@@ -45,8 +61,11 @@
             return $rtn;
         }
         
-        public static function CheckAdminExist() {
-            $res = hnd_SQLite::Query('SELECT * FROM user WHERE user_permission=:uper', array('uper' => config::PERMISSION_LEVEL_ADMIN));
+        public static function CheckAdminExist(): ?bool {
+            $res = NULL;
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': $res = hnd_SQLite::Query('SELECT * FROM user WHERE user_permission=:uper', array('uper' => config::PERMISSION_LEVEL_ADMIN)); break;
+            }
             if(is_null($res)){
                 return NULL;
             } else if (count(hnd_SQLite::ResultToArray($res)) == 0) {
@@ -56,20 +75,30 @@
             }
         }
         
-        public static function MakeAdmin($uname, $upass) {
-            if (!self::CheckAdminExist()) return self::MakeUser($uname, $upass, Config::PERMISSION_LEVEL_ADMIN);
+        public static function MakeAdmin($uname, $upass): bool {
+            if (!self::CheckAdminExist()) {
+                switch (config::DB_HANDLER) {
+                    case 'SQLITE': 
+                        self::MakeUser($uname, $upass, Config::PERMISSION_LEVEL_ADMIN); 
+                        return TRUE;
+                }
+            }
+            return FALSE;
         }
         
-        public static function GetPermission($uname) {
-            $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $uname)));
+        public static function GetPermission($uname): ?int {
+            $res = [];
+            switch (config::DB_HANDLER) {
+                case 'SQLITE': $res =  hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $uname))); break;
+            }
             if(is_null($res) or count($res) == 0){
-                return FALSE;
+                return NULL;
             } else {
                 return (int)$res[0]['user_permission'];
             }
         }
         
-        public static function CheckPermission($minlv) {
+        public static function CheckPermission($minlv): ?bool {
 			if (mdl_user::UseDB() != 0) {
 				utl_htmldoc::ShowError(500, "DB Error");
 				return NULL;
