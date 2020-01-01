b2b36828a9240ae24ffd67997378ece41b666618
Dictor <kimdictor@gmail.com>
2020-01-01 04:23:27 +0000

    Add current handler selector variable in each model class
    * handler can be selected by SetDB function which added in this commit

--- model/mdl_book.php
+++ model/mdl_book.php
@@ -16,22 +16,28 @@
 	}
 
 	class mdl_book {
+		private static $currentHandler = config::DB_HANDLER;
+		
+		public static function SetDB(string $handler): void {
+			self::$currentHandler = $handler;
+		}
+		
 		public static function UseDB(): int {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': return hnd_SQLite::Open(config::PATH_SQLITE);
                 case 'JSON': return hnd_json::Open(config::PATH_JSON);
 			}
 		}
 		
 		public static function GetAllBooks(): array {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook', array()));
                 case 'JSON': return hnd_json::ResultToComicbook(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_BOOK)->get());
 			}
 		}
 		
 		public static function AddBook(Comicbook $book): void {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE':
 					hnd_SQLite::Execute('INSERT INTO comicbook (book_path, book_name, book_author, image_count, image_size, added_date) VALUES (:bpath, :bname, :bauthor, :bimgcnt, :bimgsize, :bdate)', 
 						array('bpath' => $book->path, 'bname' => $book->name, 'bauthor' => $book->author, 'bimgcnt' => $book->imgcnt, 'bimgsize' => $book->imgsize, 'bdate' => (new \DateTime('now'))->format(\DateTime::ATOM)));
@@ -53,28 +59,28 @@
 		}
 		
 		public static function DeleteBook(Comicbook $book): void {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_id=:bid', array('bid' => $book->$id)); break;
 				case 'JSON': hnd_json::GetDB()->delete()->from(hnd_json::TABLE_BOOK)->where(['book_id' => $book->$id])->trigger(); break;
 			}
 		}
 		
 		public static function DeleteAllBooks(): void {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook', array()); break;
                 case 'JSON': hnd_json::GetDB()->delete()->from(hnd_json::TABLE_BOOK)->trigger(); break;
 			}
 		}
 		
 		public static function DeleteBookByPath(string $path): void {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('DELETE FROM comicbook WHERE book_path=:bpath', array('bpath' => $path)); break;
                 case 'JSON': hnd_json::GetDB()->delete()->from(hnd_json::TABLE_BOOK)->where(['book_path' => $path])->trigger(); break;
 			}
 		}
 		
 		public static function SearchBook(string $id): array {
-			switch (config::DB_HANDLER) {
+			switch (self::$currentHandler) {
                 case 'SQLITE': return hnd_SQLite::ResultToComicbook(hnd_SQLite::Query('SELECT * FROM comicbook WHERE book_id=:bid', array('bid' => $id)));
                 case 'JSON': return hnd_json::ResultToComicbook(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_BOOK)->where(['book_id' => $id])->get());
 			}
--- model/mdl_user.php
+++ model/mdl_user.php
@@ -3,8 +3,14 @@
     require_once 'autoload.php';
     
     class mdl_user {
+        private static $currentHandler = config::DB_HANDLER;
+        
+        public static function SetDB(string $handler): void {
+			self::$currentHandler = $handler;
+		}
+        
         public static function UseDB(): int {
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': return hnd_SQLite::Open(config::PATH_SQLITE);
                 case 'JSON': return hnd_json::Open(config::PATH_JSON);
             }
@@ -13,7 +19,7 @@
         
         public static function CheckPassword(string $user_name, string $user_pass): bool {
             $res = [];
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $user_name))); break;
                 case 'JSON': $res = hnd_json::ResultToArray(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_USER)->where(['user_name' => $user_name])->get()); break;
             }
@@ -29,28 +35,28 @@
         }
         
         public static function ChangePassword(string $user_name, string $new_pass): void {
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('UPDATE user SET user_pass = :newpass WHERE user_name = :uname', array('uname' => $user_name, 'newpass' => password_hash($new_pass, PASSWORD_DEFAULT))); break;   
                 case 'JSON': hnd_json::GetDB()->update(['user_pass' => password_hash($new_pass, PASSWORD_DEFAULT)])->from(hnd_json::TABLE_USER)->where(['user_name' => $user_name])->trigger(); break;
             }
         }
         
         public static function ChangePermission(string $user_name, int $new_permission): void {
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('UPDATE user SET user_permission = :uper WHERE user_name = :uname', array('uname' => $user_name, 'uper' => $new_permission)); break;
                 case 'JSON': hnd_json::GetDB()->update(['user_permission' => $new_permission])->from(hnd_json::TABLE_USER)->where(['user_name' => $user_name])->trigger(); break;
             }
         }
         
         public static function DeleteUser(string $user_name): void {
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('DELETE FROM user WHERE user_name = :uname', array('uname' => $user_name)); break;
                 case 'JSON': hnd_json::GetDB()->delete()->from(hnd_json::TABLE_USER)->where(['user_name' => $user_name])->trigger(); break;
             }
         }
         
         public static function MakeUser(string $user_name, string $user_pass, int $user_permission): void {
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': hnd_SQLite::Execute('INSERT INTO user VALUES (:uname, :upass, :uper);', array('uname' => $user_name, 'upass' => password_hash($user_pass, PASSWORD_DEFAULT), 'uper' => $user_permission)); break;
                 case 'JSON': 
                     hnd_json::GetDB()->insert(hnd_json::TABLE_USER,
@@ -66,7 +72,7 @@
         
         public static function GetAllUser(): array {
             $res = [];
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user', array())); break;
                 case 'JSON': $res = hnd_json::ResultToArray(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_USER)->get()); break;
             }
@@ -79,7 +85,7 @@
         
         public static function CheckAdminExist(): ?bool {
             $res = NULL;
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_permission=:uper', array('uper' => config::PERMISSION_LEVEL_ADMIN))); break;
                 case 'JSON': $res = hnd_json::ResultToArray(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_USER)->where(['user_permission' => config::PERMISSION_LEVEL_ADMIN])->get()); break;
             }
@@ -94,7 +100,7 @@
         
         public static function MakeAdmin($user_name, $user_pass): bool {
             if (!self::CheckAdminExist()) {
-                self::MakeUser($user_name, $user_pass, Config::PERMISSION_LEVEL_ADMIN); 
+                self::MakeUser($user_name, $user_pass, config::PERMISSION_LEVEL_ADMIN); 
                 return TRUE;
             }
             return FALSE;
@@ -102,7 +108,7 @@
         
         public static function GetPermission($user_name): ?int {
             $res = [];
-            switch (config::DB_HANDLER) {
+            switch (self::$currentHandler) {
                 case 'SQLITE': $res = hnd_SQLite::ResultToArray(hnd_SQLite::Query('SELECT * FROM user WHERE user_name=:uname', array('uname' => $user_name))); break;
                 case 'JSON': $res = hnd_json::ResultToArray(hnd_json::GetDB()->select('*')->from(hnd_json::TABLE_USER)->where(['user_name' => $user_name])->get()); break;
             }
