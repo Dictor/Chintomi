38f91712c96828cb5190f90b7054f98b1c87ccf0
Dictor <kimdictor@gmail.com>
2024-08-26 00:49:17 +0900

    Implement basic front end template (base, book card)

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	elogrus "github.com/dictor/echologrus"
+	"github.com/labstack/echo/v4"
+)
+
 func main() {
-	
+	e := echo.New()
+	elogrus.Attach(e)
+	e.GET("/", func(c echo.Context) error {
+		books := []Book{}
+
+		for i := 0; i < 50; i++ {
+			books = append(books, Book{
+				ID:         "",
+				Name:       fmt.Sprintf("%d", i),
+				Path:       "",
+				Author:     "",
+				ImageCount: i,
+				ImageSize:  i,
+				AddedDate:  time.Time{},
+			})
+		}
+
+		if err := BaseTemplate(BookCardTemplate(books)...).Render(c.Response().Writer); err != nil {
+			e.Logger.Error(err)
+			return c.NoContent(http.StatusInternalServerError)
+		}
+		return c.NoContent(http.StatusOK)
+	})
+	e.Logger.Fatal(e.Start(":80"))
 }
---
+++ template.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	g "github.com/maragudk/gomponents"
+	c "github.com/maragudk/gomponents/components"
+	. "github.com/maragudk/gomponents/html"
+)
+
+func BaseTemplate(content ...g.Node) g.Node {
+	return c.HTML5(c.HTML5Props{
+		Title:    "Chintomi",
+		Language: "ko",
+		Head: []g.Node{
+			Meta(Charset("utf-8")),
+			Meta(Name("viewport"), Content("width=device-width, initial-scale=1")),
+			Link(Href("https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css"), Rel("stylesheet"), Type("text/css")),
+			Script(Src("https://cdn.tailwindcss.com")),
+		},
+		Body: []g.Node{
+			Class("flex flex-col"),
+			Div(Class("grow p-2"),
+				Div(Class("navbar bg-base-100 shadow-xl rounded-box"),
+					A(Class("btn btn-ghost text-xl"), g.Text("Chintomi")),
+				)),
+			Div(append([]g.Node{Class("grow p-2 flex flex-row flex-wrap justify-around")}, content...)...),
+		},
+	})
+}
+
+func BookCardTemplate(books []Book) []g.Node {
+	return g.Map(books, func(b Book) g.Node {
+		return Div(Class("card bg-base-100 w-72 shadow-xl"),
+			Figure(Img(
+				Src("https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"),
+				Alt("Thumbnail"),
+			),
+			),
+			Div(Class("card-body"),
+				H2(Class("card-title"), g.Text("Title")),
+				Ul(Class("list-none"),
+					Li(g.Text("Page")),
+					Li(g.Text("Size")),
+				),
+				Div(Class("card-actions justify-end"),
+					Button(Class("btn btn-primary"), g.Text("Button")),
+				),
+			),
+		)
+	})
+}
